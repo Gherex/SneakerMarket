592454e4dafd1a09cfd44124a63ed99378854325
Gherex <germilagger@gmail.com>
2024-11-04 00:48:00 -0300

    A produccion

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,5 +1,5 @@
-import Carrusel from "src/carrusel/Carrusel";
-import InfoCompras from "src/infocompras/InfoCompras";
+import Carrusel from "./carrusel/Carrusel";
+import InfoCompras from "./infocompras/InfoCompras";
 
 function Inicio() {
   return (
